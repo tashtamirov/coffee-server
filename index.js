d8240ec2419708a6fc44441857ec89f877aaaa2a
tashtamirov <haron-2000@mail.ru>
2023-01-23 16:12:23 +0300

    Внес корректировки

--- index.js
+++ index.js
@@ -5,8 +5,6 @@ const app = express()
 app.use(express.json())
 app.use(require('./routes/drinks.route'))
 
-
-
 mongoose.connect('mongodb+srv://tashtamirov:Ncfuasusn9@cluster0.wdgsbkm.mongodb.net/Drinks').then(() => console.log('Успешно соединились с сервером mongoDB')).catch(() => console.log('ошибка при соединении с сервером MongoDB'))
 
 app.listen(3000, () => {
